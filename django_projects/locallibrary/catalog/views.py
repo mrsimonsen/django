e17f0f01d7744d608b945512488e0718f6b77ad3
Mr. Simonsen <johnsimonsen@weber.edu>
2019-12-02 20:50:40 -0700

    bookmark update

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre, Language
 
 def index(request):
-	'''View function for homw page of site.'''
-	
+	'''View function for home page of site.'''
+
 	# Generate counts of some of the main objects
 	num_books = Book.objects.all().count()
 	num_instances = BookInstance.objects.all().count()
-	
+
 	# Available book s(status = 'a')
 	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-	
+
 	# The 'all()' is implied by default.
 	num_authors = Author.objects.count()
-	
+
 	num_sgenre = Genre.objects.filter(name__icontains="fan").count()
 	num_sinstance = BookInstance.objects.filter(book__genre__name__icontains="fan").count()
-	
+
 	context = {
 		'num_books' : num_books,
 		'num_instances' : num_instances,
@@ -27,6 +27,11 @@ def index(request):
 		'num_sgenre': num_sgenre,
 		'num_sinstance': num_sinstance,
 	}
-	
+
 	# Render the HTML template index.html with the data in the context variable
 	return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+	model = Book
