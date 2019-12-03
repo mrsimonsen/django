e20c97cd3b9883fe29a581ec6c428cb9cbf1123b
John Simonsen <johnsimonsen@weber.edu>
2019-12-03 13:13:01 -0700

    on to part 8

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -20,7 +20,7 @@ def index(request):
 	num_sinstance = BookInstance.objects.filter(book__genre__name__icontains="fan").count()
 
 	# Number of visits to this view, as counted in the session variable.
-	num_visits =- request.session.get('num_visits', 0)
+	num_visits = request.session.get('num_visits', 0)
 	request.session['num_visits'] = num_visits + 1
 
 	context = {
