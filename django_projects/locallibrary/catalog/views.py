9b1eeb22602bc1c5e9812c76cee3ec6b5f0b975c
John Simonsen <johnsimonsen@weber.edu>
2019-12-03 11:27:50 -0700

    count update alterating between 0, -1: need to get it to work

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -19,6 +19,10 @@ def index(request):
 	num_sgenre = Genre.objects.filter(name__icontains="fan").count()
 	num_sinstance = BookInstance.objects.filter(book__genre__name__icontains="fan").count()
 
+	# Number of visits to this view, as counted in the session variable.
+	num_visits =- request.session.get('num_visits', 0)
+	request.session['num_visits'] = num_visits + 1
+
 	context = {
 		'num_books' : num_books,
 		'num_instances' : num_instances,
@@ -26,6 +30,7 @@ def index(request):
 		'num_authors' : num_authors,
 		'num_sgenre': num_sgenre,
 		'num_sinstance': num_sinstance,
+		'num_visits': num_visits,
 	}
 
 	# Render the HTML template index.html with the data in the context variable
