bbfc535da72f9061da9ae628dc49fbff75440161
John Simonsen <johnsimonsen@weber.edu>
2019-12-06 11:01:12 -0700

    bookmark update

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
 	path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 	path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 	path('borrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+	path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+	path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+	path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+	path('author/<int:pk>/update/', views.AuthorDelete.as_view(), name='author_delete'),
 	]
